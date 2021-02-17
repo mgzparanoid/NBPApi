f5e60cfcc9c88508f97a81aed8872d77e79d1e6d
mgzparanoid <31665193+mgzparanoid@users.noreply.github.com>
2021-02-17 11:21:36 +0100

    New functions implemented.

---
+++ src/main/java/pl/rkontowicz/demo/model/Currency.java
@@ -0,0 +1,60 @@
+package pl.rkontowicz.demo.model;
+
+public class Currency {
+    protected int id;
+    protected String name;
+    protected double rate;
+    protected String code;
+
+    public Currency() {
+    }
+
+    public Currency(int id, String name, double rate, String code) {
+        this.id = id;
+        this.name = name;
+        this.rate = rate;
+        this.code = code;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rate=" + rate +
+                ", code='" + code + '\'' +
+                '}';
+    }
+}
