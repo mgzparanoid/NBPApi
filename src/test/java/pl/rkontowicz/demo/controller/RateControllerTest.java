d36248238304b60a171e39e23fd59bbeb22390c5
mgzparanoid <31665193+mgzparanoid@users.noreply.github.com>
2021-03-03 22:46:42 +0100

    Added placeholders for the remaining tests from the RateController.

---
+++ src/test/java/pl/rkontowicz/demo/controller/RateControllerTest.java
@@ -0,0 +1,24 @@
+package pl.rkontowicz.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateControllerTest {
+//TODO: Add test implementations
+    @Test
+    void getConcurrencyTable() {
+    }
+
+    @Test
+    void allCurrencies() {
+    }
+
+    @Test
+    void currencyCounter() {
+    }
+
+    @Test
+    void currencyMid() {
+    }
+}
